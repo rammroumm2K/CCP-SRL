c07df71dcdf509cf32f80963bd6b6f3fbecb487b
Rami Ben Salem <rbs@ccp.be>
2024-11-19 13:36:44 +0100

    card box redesigned new svg and new background

---
+++ handmade template/js/script.js
@@ -0,0 +1,9 @@
+  // Function to set the footer year dynamically
+  function updateFooterYear() {
+    const yearSpan = document.getElementById('current-year');
+    const currentYear = new Date().getFullYear();
+    yearSpan.textContent = currentYear;
+  }
+
+  // Call the function on page load
+  updateFooterYear();
